cef6527515cf33f5a2d5498c03a0b56ddd775a8b
Luis Villalobos <luisevm24@outlook.com>
2020-06-30 02:24:38 -0500

    add dependencie navigationArgs

--- buildSrc/src/main/java/Configurations.kt
+++ buildSrc/src/main/java/Configurations.kt
@@ -6,6 +6,7 @@ object DefaultConfig {
     object Version {
         const val kotlin = "1.3.72"
         const val gradle = "4.0.0"
+        const val navigationArgs = "2.3.0"
     }
 
     const val compileSdk = 29
@@ -17,6 +18,7 @@ object DefaultConfig {
     const val gradle = "com.android.tools.build:gradle:${Version.gradle}"
     const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.kotlin}"
     const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Version.kotlin}"
+    const val navigationArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:${Version.navigationArgs}"
 }
 
 @Suppress("unused")
