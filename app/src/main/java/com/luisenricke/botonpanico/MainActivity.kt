98c9f43d7906f91775518c2329b54f76123eb344
Luis Villalobos <luisevm24@outlook.com>
2020-06-30 00:19:45 -0500

    fix binding views

--- app/src/main/java/com/luisenricke/botonpanico/MainActivity.kt
+++ app/src/main/java/com/luisenricke/botonpanico/MainActivity.kt
@@ -2,57 +2,43 @@ package com.luisenricke.botonpanico
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.luisenricke.botonpanico.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity()
-//BottomNavigationView.OnNavigationItemSelectedListener {
-{
-    /*
-        lateinit var toolbar:Toolbar
+class MainActivity : AppCompatActivity()/*,BottomNavigationView.OnNavigationItemSelectedListener*/ {
 
-        lateinit var navigation: BottomNavigationView
-
-
-     */
-    private lateinit var navController: NavController
     private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding
+    private val binding get() = _binding!!
 
-    lateinit var appBarConfiguration: AppBarConfiguration
+    private val appBarConfiguration: AppBarConfiguration by lazy {
+        AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_alert, R.id.nav_profile
+            )
+        )
+    }
 
-    val navigationIds = setOf(
-        R.id.nav_home, R.id.nav_alert, R.id.nav_profile
-    )
+    private val navController: NavController by lazy {
+        (supportFragmentManager
+            .findFragmentById(binding.fragmentHost.id) as NavHostFragment)
+            .navController
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        _binding = ActivityMainBinding.inflate(layoutInflater)
-//
-//        appBarConfiguration = AppBarConfiguration(navigationIds)
-//
-//        binding?.apply {
-//            setContentView(root)
-//            setSupportActionBar(toolbar)
-//            navController = findNavController(fragmentHost.hashCode())
-//            setupActionBarWithNavController(navController, appBarConfiguration)
-//            bottom.setupWithNavController(navController)
-//        }
+        _binding = ActivityMainBinding.inflate(layoutInflater)
 
-
-//        toolbar = findViewById(R.id.toolbar)
-//        setSupportActionBar(toolbar)
-
-//        navigation = findViewById(R.id.navigation)
-
-
+        binding.apply {
+            setContentView(root)
+            setSupportActionBar(toolbar)
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            bottom.setupWithNavController(navController)
+        }
     }
 
     override fun onStop() {
@@ -60,23 +46,7 @@ class MainActivity : AppCompatActivity()
         _binding = null
     }
 
-    /*
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.nav_first -> {
-                navController.navigate(R.id.firstFragment)
-            }
-            R.id.nav_second -> {
-                navController.navigate(R.id.secondFragment)
-            }
-            else -> return super.onOptionsItemSelected(item)
-        }
-        return true
-    }
-
     override fun onSupportNavigateUp(): Boolean {
         return navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp()
     }
-
- */
 }
--- app/src/main/java/com/luisenricke/botonpanico/presentation/HomeFragment.kt
+++ app/src/main/java/com/luisenricke/botonpanico/presentation/HomeFragment.kt
@@ -5,16 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.luisenricke.botonpanico.R
+import com.luisenricke.androidext.Toast
+import com.luisenricke.botonpanico.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.apply {
+            uno.setOnClickListener {
+                Toast.short(binding.root.context, "Uno")
+            }
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
