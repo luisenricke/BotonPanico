ce146e2c347a283edfdbf2ee12305afcc10f4763
Luis Villalobos <luisevm24@outlook.com>
2020-07-03 20:36:39 -0500

    fix toast functions to make extension functions in fragments and activities

--- androidext/src/main/java/com/luisenricke/androidext/Toast.kt
+++ androidext/src/main/java/com/luisenricke/androidext/Toast.kt
@@ -1,14 +1,25 @@
 package com.luisenricke.androidext
 
-import android.content.Context
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
-object Toast {
-    fun short(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-    }
+@Suppress("unused")
+fun AppCompatActivity.toastShort(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
 
-    fun long(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-    }
+@Suppress("unused")
+fun AppCompatActivity.toastLong(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+@Suppress("unused")
+fun Fragment.toastShort(message: String) {
+    Toast.makeText(this.context, message, Toast.LENGTH_LONG).show()
+}
+
+@Suppress("unused")
+fun Fragment.toastLong(message: String) {
+    Toast.makeText(this.context, message, Toast.LENGTH_LONG).show()
 }
