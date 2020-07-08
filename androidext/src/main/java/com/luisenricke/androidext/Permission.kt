4623a47b1b41f5bb2c1922e772ae6df1338ab78b
Luis Villalobos <luisevm24@outlook.com>
2020-07-07 20:53:34 -0500

    add Permission extension functions

--- androidext/src/main/java/com/luisenricke/androidext/Permission.kt
+++ androidext/src/main/java/com/luisenricke/androidext/Permission.kt
@@ -1,5 +1,6 @@
 package com.luisenricke.androidext
 
+import android.content.Context
 import android.content.DialogInterface
 import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.util.Log
@@ -10,8 +11,10 @@ import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.Fragment
 
+// TODO: make functions to handle list of permission
+
 @Suppress("unused")
-fun AppCompatActivity.applyPermission(
+fun AppCompatActivity.permissionApply(
     permission: String,
     requestCode: Int,
     message: String = getString(R.string.permission_message),
@@ -45,11 +48,11 @@ fun AppCompatActivity.applyPermission(
 }
 
 @Suppress("unused")
-fun AppCompatActivity.checkPermission(permission: String): Boolean =
+fun AppCompatActivity.permissionCheck(permission: String): Boolean =
     checkSelfPermission(this, permission) == PERMISSION_GRANTED
 
 @Suppress("unused")
-fun Fragment.applyPermission(
+fun Fragment.permissionApply(
     permission: String,
     requestCode: Int,
     message: String = getString(R.string.permission_message),
@@ -86,6 +89,45 @@ fun Fragment.applyPermission(
 }
 
 @Suppress("unused")
-fun Fragment.checkPermission(permission: String): Boolean =
+fun Fragment.permissionCheck(permission: String): Boolean =
     checkSelfPermission(this.context!!, permission) == PERMISSION_GRANTED
 
+@Suppress("unused")
+fun Context.permissionApply(
+    permission: String,
+    requestCode: Int,
+    message: String = getString(R.string.permission_message),
+    denied: String = getString(R.string.permission_denied)
+) {
+    val activity = this as AppCompatActivity
+
+    when {
+        checkSelfPermission(this, permission) == PERMISSION_GRANTED -> {
+            Log.d("PermissionExt", "${getString(R.string.permission_granted)} of $permission")
+        }
+
+        shouldShowRequestPermissionRationale(activity, permission) -> {
+            AlertDialog.Builder(this)
+                .setTitle(getString(R.string.permission_title))
+                .setMessage(message)
+                .setPositiveButton(getString(R.string.permission_positive_button))
+                { _: DialogInterface, _: Int ->
+                    requestPermissions(activity, arrayOf(permission), requestCode)
+                }
+                .setNegativeButton(getString(R.string.permission_negative_button))
+                { dialog: DialogInterface, _: Int ->
+                    toastShort(denied)
+                    dialog.dismiss()
+                }
+                .show()
+        }
+
+        else -> {
+            requestPermissions(activity, arrayOf(permission), requestCode)
+        }
+    }
+}
+
+@Suppress("unused")
+fun Context.permissionCheck(permission: String): Boolean =
+    checkSelfPermission(this, permission) == PERMISSION_GRANTED
