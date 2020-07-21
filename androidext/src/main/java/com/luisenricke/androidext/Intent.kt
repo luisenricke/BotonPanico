ee9956af038c81f950a48713703d9a92729bcc9d
Luis Villalobos <luisevm24@outlook.com>
2020-07-21 13:41:42 -0500

    add intent to go settings for the app

--- androidext/src/main/java/com/luisenricke/androidext/Intent.kt
+++ androidext/src/main/java/com/luisenricke/androidext/Intent.kt
@@ -2,10 +2,20 @@ package com.luisenricke.androidext
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.provider.ContactsContract
 import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+
+@Suppress("unused")
+fun Context.intentSettings() {
+    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    val uri = Uri.fromParts("package", packageName, null)
+    intent.data = uri
+    this.startActivity(intent)
+}
 
 @Suppress("unused")
 fun AppCompatActivity.intentSelectContact(requestCode: Int) {
@@ -23,5 +33,3 @@ fun Fragment.intentSelectContact(requestCode: Int) {
 fun Context.intentEnableLocation() {
     this.startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
 }
-
-
