0ec823b5b5a06d1ae4bcff6d5e902220639d9202
Luis Villalobos <luisevm24@outlook.com>
2020-07-31 00:27:11 -0500

    add functionality of select image and save it in Internal Storage

--- app/src/main/java/com/luisenricke/botonpanico/Constraint.kt
+++ app/src/main/java/com/luisenricke/botonpanico/Constraint.kt
@@ -2,13 +2,13 @@ package com.luisenricke.botonpanico
 
 @Suppress("unused")
 object Constraint {
-    const val INTENT_READ_CONTACTS_CODE = 10
-    const val PERMISSION_READ_CONTACTS_CODE = 11
+    const val INTENT_READ_CONTACTS_CODE = 1
+    const val INTENT_IMAGE_FROM_GALLERY = 2
 
-    const val PERMISSION_ACCESS_FINE_LOCATION_CODE = 20
-    const val PERMISSION_ACCESS_COARSE_LOCATION = 30
+    const val PERMISSION_READ_CONTACTS_CODE = 101
+    const val PERMISSION_ACCESS_FINE_LOCATION_CODE = 102
+    const val PERMISSION_SEND_SMS = 103
+    const val PERMISSIONS_ALERT_SERVICE = 104
 
-    const val PERMISSION_SEND_SMS = 40
-
-    const val PERMISSION_GROUP_ALERT = 100
+    const val PROFILE_PHOTO = "ProfilePhoto"
 }
--- app/src/main/java/com/luisenricke/botonpanico/presentation/ProfileFragment.kt
+++ app/src/main/java/com/luisenricke/botonpanico/presentation/ProfileFragment.kt
@@ -3,14 +3,19 @@ package com.luisenricke.botonpanico.presentation
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract.CommonDataKinds.Phone
 import android.provider.ContactsContract.Contacts
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.database.getStringOrNull
-import com.luisenricke.androidext.preferenceSet
+import com.luisenricke.androidext.*
 import com.luisenricke.botonpanico.BaseFragment
 import com.luisenricke.botonpanico.Constraint
 import com.luisenricke.botonpanico.R
@@ -32,7 +37,18 @@ class ProfileFragment : BaseFragment() {
 
         binding.apply {
 
+            val imageStorage = root.context.loadImageInternalStorage(Constraint.PROFILE_PHOTO)
+            if (imageStorage != null) imgProfile.setImageBitmap(imageStorage)
+
             contact.setOnClickListener { checkContactsPermission() }
+            btnGallery.setOnClickListener {
+                intentSelectImageFromGallery(Constraint.INTENT_IMAGE_FROM_GALLERY)
+            }
+            btnDeleteImage.setOnClickListener {
+                imgProfile.setImageResource(R.drawable.ic_baseline_person_24)
+                val isDeleted = context?.deleteImageInternalStorage(Constraint.PROFILE_PHOTO)
+                Timber.i("Is profile photo deleted? $isDeleted")
+            }
 
         }
 
@@ -47,12 +63,35 @@ class ProfileFragment : BaseFragment() {
         }
 
         when (requestCode) {
+            Constraint.INTENT_IMAGE_FROM_GALLERY -> {
+                val image = getImage(binding.root.context, data) ?: return
+                binding.imgProfile.setImageBitmap(image)
+                val isSaved = binding.root.context
+                    .saveImageInternalStorage(image, Constraint.PROFILE_PHOTO)
+                Timber.i("Photo saved: $isSaved")
+            }
             Constraint.INTENT_READ_CONTACTS_CODE -> {
                 val contact = getContact(binding.root.context, data)
                 Timber.i("contact: \n $contact")
                 this.preferenceSet("phone", contact.phone)
             }
         }
+    }
+
+    // TODO test this
+    fun getImage(context: Context, data: Intent?): Bitmap? {
+        val contentURI: Uri = data?.data!!
+        var bitmap: Bitmap? = null
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val source = ImageDecoder.createSource(context.contentResolver, contentURI)
+            bitmap = ImageDecoder.decodeBitmap(source)
+
+        } else {
+            bitmap = MediaStore.Images.Media.getBitmap(context.contentResolver, contentURI)
+        }
+
+        return bitmap!!
     }
 
     fun getContact(context: Context, data: Intent?): Contact {
