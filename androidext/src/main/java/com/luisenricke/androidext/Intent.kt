91a64cbe0455b6dca5eb52a1cac4bb207198af60
Luis Villalobos <luisevm24@outlook.com>
2020-07-31 00:20:25 -0500

    add extension function to select image from gallery

--- androidext/src/main/java/com/luisenricke/androidext/Intent.kt
+++ androidext/src/main/java/com/luisenricke/androidext/Intent.kt
@@ -4,10 +4,56 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.ContactsContract
+import android.provider.MediaStore
 import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
+// region AppCompatActivity
+@Suppress("unused")
+fun AppCompatActivity.intentSelectContact(requestCode: Int) {
+    val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+    this.startActivityForResult(intent, requestCode)
+}
+
+@Suppress("unused")
+fun AppCompatActivity.intentSelectImageFromGallery(requestCode: Int) {
+    val actionGetContent = Intent(Intent.ACTION_GET_CONTENT)
+    actionGetContent.type = "image/*"
+
+    val actionPick = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+    actionPick.type = "image/*"
+
+    val intent = Intent.createChooser(actionGetContent, "Select Image")
+    intent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(actionPick))
+
+    this.startActivityForResult(intent, requestCode)
+}
+// endregion
+
+// region Fragment
+@Suppress("unused")
+fun Fragment.intentSelectContact(requestCode: Int) {
+    val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+    this.startActivityForResult(intent, requestCode)
+}
+
+@Suppress("unused")
+fun Fragment.intentSelectImageFromGallery(requestCode: Int) {
+    val actionGetContent = Intent(Intent.ACTION_GET_CONTENT)
+    actionGetContent.type = "image/*"
+
+    val actionPick = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+    actionPick.type = "image/*"
+
+    val intent = Intent.createChooser(actionGetContent, "Select Image")
+    intent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(actionPick))
+
+    this.startActivityForResult(intent, requestCode)
+}
+// endregion
+
+// region Context
 @Suppress("unused")
 fun Context.intentSettings() {
     val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
@@ -18,18 +64,7 @@ fun Context.intentSettings() {
 }
 
 @Suppress("unused")
-fun AppCompatActivity.intentSelectContact(requestCode: Int) {
-    val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
-    this.startActivityForResult(intent, requestCode)
-}
-
-@Suppress("unused")
-fun Fragment.intentSelectContact(requestCode: Int) {
-    val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
-    this.startActivityForResult(intent, requestCode)
-}
-
-@Suppress("unused")
 fun Context.intentEnableLocation() {
     this.startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
 }
+// endregion
