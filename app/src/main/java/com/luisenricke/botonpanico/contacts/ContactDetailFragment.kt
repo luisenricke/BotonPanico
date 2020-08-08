6f8573110982220299f4795b0d16452bb04d2134
Luis Villalobos <luisevm24@outlook.com>
2020-08-08 12:51:59 -0500

    fix sketch view

--- app/src/main/java/com/luisenricke/botonpanico/contacts/ContactDetailFragment.kt
+++ app/src/main/java/com/luisenricke/botonpanico/contacts/ContactDetailFragment.kt
@@ -2,10 +2,12 @@ package com.luisenricke.botonpanico.contacts
 
 import android.os.Bundle
 import android.view.*
+import com.luisenricke.androidext.loadImageInternalStorage
 import com.luisenricke.androidext.toastLong
 import com.luisenricke.botonpanico.BaseFragment
 import com.luisenricke.botonpanico.R
 import com.luisenricke.botonpanico.databinding.FragmentContactDetailBinding
+import com.luisenricke.kotlinext.formatPhone
 
 class ContactDetailFragment : BaseFragment() {
 
@@ -34,8 +36,39 @@ class ContactDetailFragment : BaseFragment() {
             getActivityContext().setSupportActionBar(toolbar)
             setupActionBar(getActivityContext().supportActionBar, getString(R.string.contact_detail))
 
-            if (idContact != 0L) {
-                toastLong("YESSS $idContact")
+            // contact
+            val contact = database.contactDAO().get(idContact)
+
+            if (idContact == 0L || contact == null) {
+                toastLong("Something happen with the contact")
+                navController.popBackStack()
+            }
+
+            contact?.let {
+                if (contact.image.isNotEmpty()) {
+                    val image = context.loadImageInternalStorage(contact.image)
+                    imgProfile.setImageBitmap(image)
+                } else {
+                    imgProfile.setImageResource(R.drawable.ic_baseline_person_24)
+                }
+
+                lblName.text = contact.name
+                lblPhone.text = contact.phone.formatPhone(" ")
+                lblRelationship.text = contact.relationship
+
+                if (contact.message.isNotEmpty()) {
+                    lblMessage.text = contact.message
+                } else {
+                    lblMessage.text = context.getString(R.string.contact_default_message)
+                }
+
+                if (contact.isHighlighted) {
+                    lblHighlighted.text = context.getString(R.string.contact_highlighted_true)
+                    imgHighlighted.setImageResource(R.drawable.ic_baseline_favorite_24)
+                } else {
+                    lblHighlighted.text = context.getString(R.string.contact_highlighted_false)
+                    imgHighlighted.setImageResource(R.drawable.ic_baseline_favorite_border_24)
+                }
             }
         }
 
